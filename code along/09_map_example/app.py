6c2d898ecbee83fa03378c67bd227a3bb290593c
Elliott C <e.p.coyne@exeter.ac.uk>
2023-06-08 15:38:46 +0100

    updated files

---
+++ code along/09_map_example/app.py
@@ -0,0 +1,79 @@
+import ipyleaflet as L
+from htmltools import css
+from shiny import App, reactive, render, ui
+from shinywidgets import output_widget, reactive_read, register_widget
+import pandas as pd
+
+app_ui = ui.page_fluid(
+    ui.div(
+        ui.input_slider("zoom", "Map zoom level", value=8, min=1, max=18),
+        ui.output_ui("map_bounds"),
+        style=css(
+            display="flex", justify_content="center", align_items="center", gap="2rem"
+        ),
+    ),
+    output_widget("map"),
+)
+
+
+def server(input, output, session):
+    # Initialize and display when the session starts (1)
+    map = L.Map(center=(50.435, -5.09426),
+                zoom=8,
+                scroll_wheel_zoom=True)
+
+    # Add Markers
+    clinics = [['BODMIN', 'PL31 2QT', (50.46754, -4.74115)], 
+               ['RCHT', 'TR1 3LQ', (50.26656, -5.09426)],
+               ['CRCH', 'TR15 3ER', (50.22938, -5.24438)],
+               ['SACH', 'PL26 6AA', (50.32714, -4.77305)],
+               ['WCH', 'TR18 2PF', (50.12205, -5.54298)]]
+
+    df_clinics = pd.DataFrame(clinics)
+    df_clinics.columns = ['Clinic', 'postcode', 'Coords']
+
+    icon = L.AwesomeIcon(
+        name='star',
+        marker_color='green',
+        icon_color='white',
+        spin=False
+    )
+
+    for index, row in df_clinics.iterrows():
+        marker = L.Marker(location=row.Coords,
+                          title=row.Clinic,
+                          icon=icon,
+                          draggable=False)
+        map.add_layer(marker);
+
+    
+    # Add a distance scale
+    map.add_control(L.leaflet.ScaleControl(position="bottomleft"))
+    register_widget("map", map)
+
+    # When the slider changes, update the map's zoom attribute (2)
+    @reactive.Effect
+    def _():
+        map.zoom = input.zoom()
+
+    # When zooming directly on the map, update the slider's value (2 and 3)
+    @reactive.Effect
+    def _():
+        ui.update_slider("zoom", value=reactive_read(map, "zoom"))
+
+    # Everytime the map's bounds change, update the output message (3)
+    @output
+    @render.ui
+    def map_bounds():
+        center = reactive_read(map, "center")
+        if len(center) == 0:
+            return
+
+        lat = round(center[0], 4)
+        lon = (center[1] + 180) % 360 - 180
+        lon = round(lon, 4)
+
+        return ui.p(f"Latitude: {lat}", ui.br(), f"Longitude: {lon}")
+
+
+app = App(app_ui, server)
